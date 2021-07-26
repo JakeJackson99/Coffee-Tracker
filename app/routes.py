f2af840c7a33efa901d9edc495ca49671c7c524a
JakeJackson99 <jakecjackson99@gmail.com>
2021-07-26 23:54:03 +0100

    Add thumnbnail for bean info on index page

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,17 @@
 from flask import render_template
-from flask_login import current_user, login_user, login_required
+from flask_login import login_required
 
 from app import app
-from app.models import AdminUser, Bean
+from app.models import Bean
 
 
 @app.route('/')
 def index():
+    beans = Bean.query.all()
     return render_template('index.html', beans=beans)
 
+
+@app.route('/admin')
+@login_required
+def admin():
+    pass
